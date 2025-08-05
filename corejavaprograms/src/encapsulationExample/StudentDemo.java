347cc08a5729c393aab8eabef1f89a49be1c58c3
kavya <kavya@Oviya>
2025-08-05 16:23:40 +0530

    New Changes Updated

---
+++ corejavaprograms/src/encapsulationExample/StudentDemo.java
@@ -0,0 +1,24 @@
+package encapsulationExample;
+
+public class StudentDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s=new Student();
+		
+		s.setSid(101);
+		s.setSname("Oviya");
+		s.setAvg(89.5f);
+		
+		System.out.println(s);
+		
+        Student s1=new Student();
+		
+		s1.setSid(102);
+		s1.setSname("Leelaa");
+		s1.setAvg(89.7f);
+		
+		System.out.println(s1);
+		
+	}
+
+}
