79adb1e86afe1b787ae3fc0e8c07985453ca9d52
kavya <kavya@Oviya>
2025-08-19 11:34:36 +0530

    Updated

---
+++ corejavaprograms/src/bufferedreader_example/FileReaderDemo.java
@@ -0,0 +1,29 @@
+package bufferedreader_example;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+
+public class FileReaderDemo {
+
+	public static void main(String[] args) throws IOException {
+		URL resource=FileReaderDemo.class.getResource("c70students.txt");
+		
+		if(resource==null) {
+			System.err.println("File not found");
+			return;
+		}
+		
+		File file = new File(resource.getFile());
+		FileReader fileReader = new FileReader(file);
+	    BufferedReader reader = new BufferedReader (fileReader);
+	    String line;
+	    while ((line = reader.readLine()) != null) {
+	    	System.out.println(line);
+		}
+		reader.close();
+	}
+
+}
