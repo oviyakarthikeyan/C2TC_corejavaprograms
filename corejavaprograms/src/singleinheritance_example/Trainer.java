cedcab7d20efe53c27723f6097a9de8d58fc2286
kavya <kavya@Oviya>
2025-08-19 12:15:27 +0530

    Updated

---
+++ corejavaprograms/src/singleinheritance_example/Trainer.java
@@ -0,0 +1,37 @@
+package singleinheritance_example;
+
+public class Trainer extends Person{
+	private int eid;
+	private String designation;
+	private String location;
+	//Parameterized constructor
+	public Trainer(int pid, String name, String city, int eid, String designation, String location) {
+		super(pid, name, city);// calling Person class constructor
+		this.eid = eid;
+		this.designation = designation;
+		this.location = location;
+	}
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	@Override
+	public String toString() {
+		return "Trainer [eid=" + eid + ", designation=" + designation + ", location=" + location + "]";
+	}
+	
+}
