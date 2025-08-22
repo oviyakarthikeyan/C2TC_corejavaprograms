f93b3e91f9a91f62af2fcbda0428fd4ec173c1ed
oviyakarthikeyan <oviyakarthikeyan29@gmail.com>
2025-08-22 14:44:00 +0530

    Updated

---
+++ corejavaprograms/src/com/tnsif/daynine/StringSubClass.java
@@ -0,0 +1,10 @@
+//can't inherit final classes
+package com.tnsif.daynine;
+
+public class StringSubClass  { //	extends String // extending wrapper which is always final #error
+
+	public static void main(String[] args) {
+		
+
+	}
+}
