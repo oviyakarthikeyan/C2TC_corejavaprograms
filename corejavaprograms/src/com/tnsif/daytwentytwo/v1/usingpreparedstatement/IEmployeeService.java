92000ad796d1b3e299515d98bd757b6de2f2def9
oviyakarthikeyan <oviyakarthikeyan29@gmail.com>
2025-09-01 10:05:23 +0530

    Updated

---
+++ corejavaprograms/src/com/tnsif/daytwentytwo/v1/usingpreparedstatement/IEmployeeService.java
@@ -0,0 +1,15 @@
+//program to define interface for CRUD operation
+package com.tnsif.daytwentytwo.v1.usingpreparedstatement;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IEmployeeService {
+	
+	List<Employee> getAllEmployees() throws SQLException;
+	Employee getEmployeeById(int empId) throws SQLException;
+	void addEmployee(Employee e)throws SQLException;
+	boolean deleteEmployee(int empId) throws SQLException;
+	boolean updateEmployee(int empId, Employee e) throws SQLException;
+	
+}
